cfe9682c0619246dd19a90d82462271c6d5f5376
Funky09 <shah2703@gmail.com>
2023-10-13 16:04:49 +0530

    Ec2 ubuntu setup

--- application.py
+++ application.py
@@ -1,19 +1,67 @@
 
-from flask import Flask, render_template, Response, request
+# from flask import Flask, render_template, Response, request
+# import time
+
+# application = Flask(__name__)
+# app = application
+
+# generate_pnl_flag = False  # Flag to control PNL generation
+
+# # Function to continuously generate a number (simulating PNL)
+# def generate_pnl():
+#     pnl = 0
+#     while generate_pnl_flag:
+#         pnl += 1  # Update the pnl value here as needed
+#         yield f"data: {pnl}\n\n"
+#         time.sleep(1)  # Adjust the sleep duration as needed
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/stream_pnl')
+# def stream_pnl():
+#     return Response(generate_pnl(), content_type='text/event-stream')
+
+# @app.route('/start_pnl', methods=['POST'])
+# def start_pnl():
+#     global generate_pnl_flag
+#     generate_pnl_flag = True
+#     return "Live PNL started."
+
+# @app.route('/stop_pnl', methods=['POST'])
+# def stop_pnl():
+#     global generate_pnl_flag
+#     generate_pnl_flag = False
+#     return "Live PNL stopped."
+
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0",port=8080)
+
+
+
+
+from flask import Flask, render_template, Response, request, session
+from flask_session import Session
 import time
 
 application = Flask(__name__)
 app = application
 
-generate_pnl_flag = False  # Flag to control PNL generation
+# Configure session to use a server-side session with a secret key
+app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with your own secret key
+app.config['SESSION_TYPE'] = 'filesystem'  # You can use other session types if needed
+Session(app)
 
 # Function to continuously generate a number (simulating PNL)
 def generate_pnl():
-    pnl = 0
-    while generate_pnl_flag:
-        pnl += 1  # Update the pnl value here as needed
-        yield f"data: {pnl}\n\n"
+    if 'pnl' not in session:
+        session['pnl'] = 0  # Initialize the PNL for the user
+    while session.get('generate_pnl_flag', False):
+        session['pnl'] += 1  # Update the user's PNL value here as needed
+        yield f"data: {session['pnl']}\n\n"
         time.sleep(1)  # Adjust the sleep duration as needed
+
 
 @app.route('/')
 def index():
@@ -25,15 +73,13 @@ def stream_pnl():
 
 @app.route('/start_pnl', methods=['POST'])
 def start_pnl():
-    global generate_pnl_flag
-    generate_pnl_flag = True
+    session['generate_pnl_flag'] = True
     return "Live PNL started."
 
 @app.route('/stop_pnl', methods=['POST'])
 def stop_pnl():
-    global generate_pnl_flag
-    generate_pnl_flag = False
+    session['generate_pnl_flag'] = False
     return "Live PNL stopped."
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=8080)
